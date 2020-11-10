d6d23172ecbd99d4a5c070ead15adb21268034cd
Rocky <yangrocky2013@gmail.com>
2020-11-10 11:05:14 -0600

    study lists

---
+++ lists.py
@@ -0,0 +1,13 @@
+# Lists
+# - declaration & length
+# - accessing items
+# - sorting
+# - sum, min, max
+
+empty = []
+print(empty)
+
+friends = ["Sammy", "Dean", "Castiel", "Bobby", "Crowley", "Chuck", "Jack"]
+print(friends)
+
+print(friends[2]) 
